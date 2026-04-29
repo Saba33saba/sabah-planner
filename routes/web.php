dd7c023ef4b18cea52de3a1d038f2a2597dbb18a
sabah <aa8057430@gmail.com>
2026-04-30 02:26:33 +0300

    Add migrate route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/welcome', function () {
     return view('welcome');
@@ -11,5 +12,11 @@ Route::resource('tasks', TaskController::class);
 Route::get('tasks/{task}/toggle', [TaskController::class, 'toggleStatus'])
     ->name('tasks.toggle');
 
+
+
+Route::get('/run-migrate', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migrations executed successfully!';
+});
 
 
